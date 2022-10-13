0d111f9626ea6e2318d310513ab17ad55ea6744f
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2022-10-14 01:58:18 +0200

    solution

---
+++ lab1/utils.py
@@ -0,0 +1,109 @@
+import logging
+import heapq
+import numpy as np
+from typing import Callable
+
+class PriorityQueue:
+    """A basic Priority Queue with simple performance optimizations"""
+
+    def __init__(self):
+        self._data_heap = list()
+        self._data_set = set()
+
+    def __bool__(self):
+        return bool(self._data_set)
+
+    def __contains__(self, item):
+        return item in self._data_set
+
+    def push(self, item, p=None):
+        assert item not in self, f"Duplicated element"
+        if p is None:
+            p = len(self._data_set)
+        self._data_set.add(item)
+        heapq.heappush(self._data_heap, (p, item))
+
+    def pop(self):
+        p, item = heapq.heappop(self._data_heap)
+        self._data_set.remove(item)
+        return item
+
+class State:
+    def __init__(self, data: np.ndarray):
+        self._data = data.copy()
+        self._data.flags.writeable = False
+
+    def __hash__(self):
+        return hash(bytes(self._data))
+
+    def __eq__(self, other):
+        return bytes(self._data) == bytes(other._data)
+
+    def __lt__(self, other):
+        return bytes(self._data) < bytes(other._data)
+
+    def __str__(self):
+        return str(self._data)
+
+    def __repr__(self):
+        return repr(self._data)
+
+    @property
+    def data(self):
+        return self._data
+
+    def copy_data(self):
+        return self._data.copy()
+
+def search(
+    initial_state: State,
+    goal_test: Callable,
+    parent_state: dict,
+    state_cost: dict,
+    priority_function: Callable,
+    unit_cost: Callable,
+    possible_actions: Callable,
+    result: Callable
+):
+    frontier = PriorityQueue()
+    parent_state.clear()
+    state_cost.clear()
+
+    state = initial_state
+    parent_state[state] = None
+    state_cost[state] = 0
+
+    while state is not None and not goal_test(state):
+        logging.debug(f"Entering in state: {state.data}")
+        logging.debug(f"Building frontier...")
+        # Build the frontier
+        for a in possible_actions(state):
+            
+            new_state = result(state, a)
+            cost = unit_cost(a)
+
+            if new_state not in state_cost and new_state not in frontier:
+                parent_state[new_state] = state
+                state_cost[new_state] = state_cost[state] + cost
+                frontier.push(new_state, p=priority_function(new_state))
+                logging.debug(f"Added new node to frontier (cost={state_cost[new_state]})")
+            elif new_state in frontier and state_cost[new_state] > state_cost[state] + cost:
+                old_cost = state_cost[new_state]
+                parent_state[new_state] = state
+                state_cost[new_state] = state_cost[state] + cost
+                logging.debug(f"Updated node cost in frontier: {old_cost} -> {state_cost[new_state]}")
+
+        # Get the next node to visit
+        if frontier:
+            state = frontier.pop()
+        else:
+            state = None
+
+    path = list()
+    s = state
+    while s:
+        path.append(s.copy_data())
+        s = parent_state[s]
+
+    logging.info(f"Found a solution in {len(path):,} steps; visited {len(state_cost):,} states")
+    return list(reversed(path))
